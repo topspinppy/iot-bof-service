66115e6ad812019d4d086f01210b11f02d97b2a1
Paranat Klinhom <tl.topspin.48@gmail.com>
2024-06-18 20:29:40 +0700

    📃document: Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigSchema } from './config.schema';
+
+function useSwagger(app: INestApplication) {
+  const config = new DocumentBuilder().setTitle('IOT Backoffice API').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => {
-    Logger.log(`Server is running at port 3000`);
+  const configService = app.get<ConfigService<ConfigSchema>>(ConfigService);
+
+  // Swagger Document
+  useSwagger(app);
+
+  const port = configService.get<number>('PORT', 4000);
+  await app.listen(port, () => {
+    Logger.log(`Server is running at port ${port}`);
   });
 }
 
