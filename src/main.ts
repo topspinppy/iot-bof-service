8eff54654dd17b6d408aa2065e19371524b0a0fc
Paranat Klinhom <tl.topspin.48@gmail.com>
2024-06-18 17:34:46 +0700

    🎉feat: Initial project (nestjs)

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000, () => {
+    Logger.log(`Server is running at port 3000`);
+  });
+}
+
+void bootstrap();
